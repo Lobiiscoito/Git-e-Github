290c92e8299adeecbfbf08f0be476b1752bfeb7b
Lobiiscoito <lobiiscoito@gmail.com>
2023-04-14 09:36:09 -0300

    aaa

--- jogo/forca2.py
+++ jogo/forca2.py
@@ -1,41 +1,168 @@
+import random
+
 def jogar2():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
-
-    palavra_secreta = "maça".upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
-
-    enforcou = False
-    acertou = False
-    erros = 0
-
-    print(letras_acertadas)
-
-    while(not enforcou and not acertou):
-
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
-
-        if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-            erros += 1
-
-        enforcou = erros == 6
-        acertou = "_" not in letras_acertadas
+    boas_vindas()
+    while True:
+        palavra_secreta = carregar_palavra_secreta()
+        letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
         print(letras_acertadas)
+        
+
+        enforcou = False
+        acertou = False
+        erros = 0
 
 
-    if(acertou):
-        print("Você ganhou!!")
-    else:
-        print("Você perdeu!!")
-    print("Fim do jogo")
+        while(not enforcou and not acertou):
+            print()
+            chute = solicitacao_do_chute()
+
+            if(chute in palavra_secreta):
+                corrigindo_chute(chute, palavra_secreta, letras_acertadas)
+            else:
+                erros += 1
+                desenha_forca(erros)
+
+            enforcou = erros == 7
+            acertou = "_" not in letras_acertadas
+            print(letras_acertadas)
+
+
+        if(acertou):
+            mensagem_vencedor()
+        else:
+            mensagem_perdedor(palavra_secreta)
+        print()
+        print("Fim do jogo")
+       
+       # opção se quer continuar
+       
+        while True:
+            continuar= str(input("Deseja continuar [S] ou [N]: ")).upper()
+            if continuar in 'SN':
+                    break
+        if continuar in 'Nn':
+            break
+        
+def boas_vindas():
+        print("*************************************")
+        print("**** Bem vindo ao jogo da Forca! ****")
+        print("*************************************")
+
+def carregar_palavra_secreta():
+    with open(r'C:\Users\lobii\OneDrive\Área de Trabalho\VS code\Python\Jogos\palavras.txt',) as arquivo:
+        palavras = []
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+
+        arquivo.close()
+
+        numero = random.randrange(0, len(palavras))
+
+        print()
+        palavra_secreta = palavras[numero].upper()
+        return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+def solicitacao_do_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+    return chute
+
+def corrigindo_chute(chute, palavra_secreta, letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+        if(chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+
+def mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+def mensagem_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print(f"A palavra era {palavra_secreta}")
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
 
 if(__name__ == "__main__"):
     jogar2()
--- jogo/jogar.py
+++ jogo/jogar.py
@@ -1,19 +1,31 @@
 import advinhacao
 import forca2
+from colorama import init, Fore, Back, Style
 
-print("-="*30)
-print("********* ESCOLHA UM JOGO *********")
-print("-="*30)
-jogo=int(input("(1) Advinhação (2) Forca    (3) : "))
+# Inicializar a biblioteca colorama
+init()
 
-if jogo == 1:
-    print("Jogando Advinhação")
-    advinhacao.jogar()
-if jogo == 2:
-    print("Jogando forca")
-    forca2.jogar2()
 
-print("-="*30)
-print("********* ESCOLHA UM JOGO *********")
-print("-="*30)
-jogo=int(input("(1) Advinhação (2) Forca    (3) : "))
+while True:
+    print(Fore.YELLOW+"-="*25)
+    print("      *********** ", end = '')
+    print(Style.RESET_ALL, end='')
+    print("ESCOLHA UM JOGO ", end = '' )
+    print(Fore.YELLOW+ "***********               ")
+    print("-="*25)
+    print(Style.RESET_ALL)
+    while True:
+        print("      (1) Advinhação", end = " / ")
+        print("(2) Forca", end = " / ")
+        print(Fore.RED+ "(3) SAIR ")
+        print(Style.RESET_ALL)
+        jogo=int(input(":"))
+        if jogo == 1:
+            advinhacao.jogar()
+        if jogo == 2:
+            forca2.jogar2()
+        if jogo == 3:
+            break
+    break
+
+
